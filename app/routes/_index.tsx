717e0129b5b75f0cf3b6a655ae007d0088f7732f
Sergey Veremiychuk <veremey4uk@gmail.com>
2023-08-07 17:23:12 +0200

    init supabese | get data

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,13 @@
-import type { V2_MetaFunction } from "@remix-run/node";
+import { useLoaderData } from '@remix-run/react'
+import supabase from 'utils/supabase'
 
-export const meta: V2_MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
+export const loader = async () => {
+	const { data } = await supabase.from('messages').select()
+	return { data }
+}
 
 export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+	const { data } = useLoaderData()
+
+	return <pre>{JSON.stringify(data, null, 2)}</pre>
 }
