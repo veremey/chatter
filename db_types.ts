6b76eaa46190dea0f887a16d07b3fa1304acec08
Sergey Veremiychuk <veremey4uk@gmail.com>
2023-08-07 17:42:46 +0200

    het typed data from DB | use typed data

---
+++ db_types.ts
@@ -0,0 +1,44 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
+
+export interface Database {
+  public: {
+    Tables: {
+      messages: {
+        Row: {
+          content: string
+          created_at: string
+          id: string
+        }
+        Insert: {
+          content: string
+          created_at?: string
+          id?: string
+        }
+        Update: {
+          content?: string
+          created_at?: string
+          id?: string
+        }
+        Relationships: []
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,3 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
-export default createClient(process.env.SUPABASE_URL!, process.env.SUPABASE_ANON_KEY!)
+import type { Database } from 'db_types'
+
+export default createClient<Database>(
+	process.env.SUPABASE_URL!,
+	process.env.SUPABASE_ANON_KEY!
+)
